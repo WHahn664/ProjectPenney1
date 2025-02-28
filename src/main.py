46662fec8b6427bb70143a86ed36a06d0ff721ae
WHahn664 <152346599+WHahn664@users.noreply.github.com>
2025-02-28 12:06:48 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from processing import play_all_games
 from visualization import visualize_heatmap
 
-# Run Simulation
+# Running Simulation for 100000 decks
 num_decks_to_generate = 100000
 results, seeds = play_all_games(num_decks_to_generate)
 
-# Visualize heatmaps for Player 2 probabilities and Draws
+# Creating heatmaps for Player 2 probabilities
 visualize_heatmap(results, ["Player 2 Win %", "Draw %"])
