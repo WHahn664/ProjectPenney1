0baddd24f8cd62cc9a7283786c881e6c4496c33d
WHahn664 <152346599+WHahn664@users.noreply.github.com>
2025-03-01 23:04:37 -0500

    Delete src/main.py

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from processing import play_all_games
-from visualization import visualize_heatmap
-
-# Running Simulation for 100000 decks
-num_decks_to_generate = 100000
-results, seeds = play_all_games(num_decks_to_generate)
-
-# Creating heatmaps for Player 2 probabilities
-visualize_heatmap(results, ["Player 2 Win %", "Draw %"])
