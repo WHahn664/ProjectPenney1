61bd2ba84f0b8ac02171a505851dfc0b821b0854
WHahn664 <152346599+WHahn664@users.noreply.github.com>
2025-03-28 23:11:56 -0400

    Delete src/test.py

--- src/test.py
+++
@@ -1,9 +0,0 @@
-from processing import simulate_games
-from visualization import visualize_heatmap
-
-# This will simulate 100000 games of penney
-num_decks_to_generate = 100000
-results, seeds = simulate_games(num_decks_to_generate)
-
-# This will visualize our results with heatmaps
-visualize_heatmap(results, ["Player 2 Win % (Trick)", "Player 2 Win % (Total)"])
