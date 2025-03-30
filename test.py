fea8b9951b8f4c8006551c82e36640421be683c8
WHahn664 <152346599+WHahn664@users.noreply.github.com>
2025-03-30 03:44:24 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,18 +2,15 @@ from processing import simulate_games
 from visualization import visualize_heatmap
 
 # This will simulate 1000000 games of penney
-num_decks_to_generate = 1000000
-results1, seeds1 = simulate_games(num_decks_to_generate)
-
-# This will visualize our results with heatmaps
-visualize_heatmap(results1, ["Player 2 Win % (Trick)", "Player 2 Win % (Total)"])
-
-# Augment existing data with more decks (put any number of decks in num_decks). Comment out all of the lines of code below.
+results1, seeds1 = simulate_games(num_decks=1000000)
+visualize_heatmap(results1, ["Player 2 Win % (Trick)", "Player 2 Win % (Total)"], num_decks=1000000)
+#Augment with more decks (put any number of decks in num_decks). Uncomment all the code below. Then comment the two lines of code above.
+#Also, update num_decks in visualize_heatmap to the expected amount of total decks each time you want to add new decks.
 #results2, seeds2 = simulate_games(
-#    num_decks=100,
-#    existing_results=results1,
-#    existing_seeds=seeds1,
-#    save_prefix="test_aug",
-#    save_to_file=True
+    #num_decks=100,
+    #existing_results=results1,
+    #existing_seeds=seeds1
+    
 #)
-# visualize_heatmap(results2, ["Player 2 Win % (Trick)", "Player 2 Win % (Total)"])
+
+#visualize_heatmap(results2, ["Player 2 Win % (Trick)", "Player 2 Win % (Total)"], num_decks=1000100)
