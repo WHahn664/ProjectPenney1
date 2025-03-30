eac2958dd3925b6c30101d92a66fd535b4a3ac9c
WHahn664 <152346599+WHahn664@users.noreply.github.com>
2025-03-30 05:23:44 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ from visualization import visualize_heatmap
 results1, seeds1 = simulate_games(num_decks=1000000)
 visualize_heatmap(results1, ["Player 2 Win % (Trick)", "Player 2 Win % (Total)"], num_decks=1000000)
 # Augment with more decks by setting num_decks to the desired amount. Make sure to run the two lines of code before doing this.
-# Uncomment the whole code below and comment out the two lines above.
+# Uncomment the whole code below and comment out the two lines of code above.
 # Also, update num_decks in visualize_heatmap to reflect the total number of decks each time you add new ones.
 #results2, seeds2 = simulate_games(
     #num_decks=100,
